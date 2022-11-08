3f96b3d917279426163f450f23d2422728b2ed16
Clemens <clemens.ott@outlook.de>
2022-11-08 06:31:17 +0100

    add logging of unhandled exception

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace PictureView
 {
@@ -7,5 +9,16 @@ namespace PictureView
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            DispatcherUnhandledException += OnDispatcherUnhandledException;
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            File.WriteAllText("./UnhandledException.txt", e.Exception.ToString());
+        }
     }
 }
