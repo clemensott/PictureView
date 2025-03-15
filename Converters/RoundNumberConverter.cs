bb40679a07bc711151cfa43747a5a190b7d328b2
Clemens <clemens.ott@outlook.de>
2025-03-15 12:28:02 +0100

    replace NotImplementedException with NotSupportedException

--- Converters/RoundNumberConverter.cs
+++ Converters/RoundNumberConverter.cs
@@ -18,6 +18,6 @@ public class RoundNumberConverter : IValueConverter
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 }
